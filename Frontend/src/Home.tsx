ff98847ea84258f83d0ddb59f93bd184df5e0f41
lazy34 <maitysurajit0901@gmail.com>
2024-04-06 00:50:34 +0530

    homepage -header layput done

--- Frontend/src/Home.tsx
+++ Frontend/src/Home.tsx
@@ -1,9 +1,25 @@
+// import styled from "styled-components";
+import "./styles/Home.css";
 
+export const Home = () => {
+  return (
+    <>
+      <div className="blog-container">
+        <div className="blog-logo-greet-visit">
+            <div className="blog-inside-header">
+                <div className="blog-logo">
 
-export const Home=()=>{
-    return(
-        <>
-            <h1>This is the Home Page</h1>
-        </>
-    );
-}
+                </div>
+                <div className="blog-greet">
+                    <p>Good <span>Morning</span></p>
+                </div>
+                <div className="blog-visit-profile-btn">
+                    <p>Visit my Profile</p>
+                </div>
+            </div>
+        </div>
+        <div className="blog-catagories"></div>
+      </div>
+    </>
+  );
+};
